57b181250e4c54cbb5b8bd1e7637eb3a2650d1b6
Dudas Akos <dudas.akos@vik.bme.hu>
2022-04-28 11:50:51 +0200

    Upgrade Users to Python 3.10

--- src/Users/app.py
+++ src/Users/app.py
@@ -6,11 +6,11 @@ import os
 import socket
 
 # Development and production behavior might vary; decide runtime
-is_development = os.environ.get('TODOAPP_IsDevelopment') is not None
+is_development = os.environ.get("TODOAPP_IsDevelopment") is not None
 
 # Get connection string from environment variable
-mongo_url = os.getenv('TODOAPP_MongoUrl', 'mongodb://mongodb:27017')
-redis_url = os.getenv('TODOAPP_RedisUrl', 'redis')
+mongo_url = os.getenv("TODOAPP_MongoUrl", "mongodb://mongodb:27017")
+redis_url = os.getenv("TODOAPP_RedisUrl", "redis")
 
 mongo = MongoClient(mongo_url)
 redis = Redis(host=redis_url, port=6379)
@@ -20,50 +20,51 @@ app = Flask(__name__)
 if is_development:
     # Allow CORS; makes frontend development easier; must not be used in production
     cors = CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.config["CORS_HEADERS"] = "Content-Type"
 
 # List all users
-@app.route("/api/users", methods=['GET'])
+@app.route("/api/users", methods=["GET"])
 @cross_origin()
 def get_all_users():
     users_collection = mongo.todoapp.users
     output = []
     for u in users_collection.find():
-        output.append({'name': u['name'], 'id': u['id']})
+        output.append({"name": u["name"], "id": u["id"]})
     return jsonify(output)
 
 
 # Get a particular user
-@app.route("/api/users/<int:id>", methods=['GET'])
+@app.route("/api/users/<int:id>", methods=["GET"])
 @cross_origin()
 def get_user(id):
     users_collection = mongo.todoapp.users
-    u = users_collection.find_one({'id': id})
+    u = users_collection.find_one({"id": id})
     if u:
-        output = {'name': u['name'], 'id': u['id']}
+        output = {"name": u["name"], "id": u["id"]}
         return jsonify(output)
     else:
-        return make_response(jsonify({'error': 'Not found'}), 404)
+        return make_response(jsonify({"error": "Not found"}), 404)
+
 
 # Delete a particular user
-@app.route("/api/users/<int:id>", methods=['DELETE'])
+@app.route("/api/users/<int:id>", methods=["DELETE"])
 @cross_origin()
 def delete_user(id):
     users_collection = mongo.todoapp.users
-    result = users_collection.delete_one({'id': id})
+    result = users_collection.delete_one({"id": id})
     if result.deleted_count > 0:
-        redis.sadd('DeleteUserQueue', str(id))
-        return make_response('OK', 200)
+        redis.sadd("DeleteUserQueue", str(id))
+        return make_response("OK", 200)
     else:
-        return make_response(jsonify({'error': 'Not found'}), 404)
+        return make_response(jsonify({"error": "Not found"}), 404)
 
 
 # Decide if the caller is authenticated; used by forward authentication
-@app.route("/api/auth", methods=['GET'])
+@app.route("/api/auth", methods=["GET"])
 def get_is_auth():
     # TODO 7. feladat
     # return make_response(jsonify({'error': 'Unauthorized'}), 401)
-    return make_response(jsonify({'auth': 'ok'}), 200)
+    return make_response(jsonify({"auth": "ok"}), 200)
 
 
 if __name__ == "__main__":
@@ -71,12 +72,9 @@ if __name__ == "__main__":
     # Add sample data
     if is_development:
         users_collection = mongo.todoapp.users
-        users_collection.replace_one(
-            {'id': 1}, {'id': 1, 'name': 'Alma'}, True)
-        users_collection.replace_one(
-            {'id': 2}, {'id': 2, 'name': 'Banan'}, True)
-        users_collection.replace_one(
-            {'id': 3}, {'id': 3, 'name': 'Citrom'}, True)
+        users_collection.replace_one({"id": 1}, {"id": 1, "name": "Alma"}, True)
+        users_collection.replace_one({"id": 2}, {"id": 2, "name": "Banan"}, True)
+        users_collection.replace_one({"id": 3}, {"id": 3, "name": "Citrom"}, True)
 
     # Start the Flash host
-    app.run(host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0", port=80)
